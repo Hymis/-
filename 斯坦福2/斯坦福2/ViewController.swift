48b1fb283add29bf0d4f6312a07e9fb323bd11be
上海海洋大学 <shanhaikaiyoudaigaku@shanghaihaiyangdaxuedeimac-3.local>
2017-03-31 20:24:36 +0800

    有根号和π的Calculator

---
+++ 斯坦福2/斯坦福2/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  斯坦福2
+//
+//  Created by 上海海洋大学 on 17/3/31.
+//  Copyright © 2017年 上海海洋大学. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    
+    @IBOutlet weak var display: UILabel!
+    var  userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping{
+       let textCurrentlyInDisplay = display.text!
+        display.text = textCurrentlyInDisplay + digit
+        }else {
+            display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+}
+    var displayValue:Double{
+        get{
+            return Double(display.text!)!
+        }
+            set{
+                display.text = String(newValue)
+            }
+    
+    }
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol{
+                case "π":
+                display.text = String(Double.pi)
+                case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+        
+    }
+    
+    
+    
+}
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
